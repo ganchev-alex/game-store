5b6dc9648bb6f9bcb83684a2af994d4f059462f3
AGanchev19 <130899140+ganchev-alex@users.noreply.github.com>
2024-09-25 17:18:27 +0300

    Project Set Up.
    The starting point of the project - I have set up all of the file modules that I will need to build my site on. I have provided a couple of global styling sheets and some additional files for the creation of the navigation.

---
+++ components/navigation_bars/PrimaryNavigationBar.tsx
@@ -0,0 +1,40 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+
+import styles from "./PrimaryNavigation.module.scss";
+
+import tokenSrc from "../../public/assets/icons/token.png";
+import { usePathname } from "next/navigation";
+
+const PrimaryNavigationBar: React.FC = function () {
+  const path = usePathname();
+
+  const linkData = [
+    { label: "Store", path: "/store" },
+    { label: "Cart", path: "/cart" },
+    { label: "About", path: "/about" },
+  ];
+
+  return (
+    <nav className={styles.navigation}>
+      <Link href={"/"} className={styles["navigation__home"]}>
+        <Image src={tokenSrc} alt="Site Logo" width={50} height={50} />
+        <h3>Game Store</h3>
+      </Link>
+      {linkData.map((set, key) => (
+        <Link
+          key={key}
+          href={set.path}
+          className={`${styles["navigation__link"]} ${
+            path === set.path ? styles["navigation__link--active"] : ""
+          }`}
+        >
+          {set.label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default PrimaryNavigationBar;
